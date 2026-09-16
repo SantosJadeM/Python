807a09ecf2a5ecebb8f2e53a7bdd95193d6532cf
Santos Jade <Jadesantos936@gmail.com>
2026-09-16 19:02:49 +0800

    Update Compound Interest Calculator

--- Projects/Compound Interest Calculator.py
+++ Projects/Compound Interest Calculator.py
@@ -23,4 +23,3 @@ while time <= 0:
 total = principle * pow((1+ rate/100), time)
 print ("---------------------------------------------------------")
 print (f"The total amount after {time} years is ${total:.2f}")
-print ("---------------------------------------------------------")
